7448eba567ab26e31f95063af324a361b0117a71
Mariabelfort <mariabelfort@hotmail.com>
2023-05-15 08:20:39 -0500

    actualizacion 15 mayo

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function darBienvenida ()
+{
+   let bienvenida = "HOLA";
+   console.log (bienvenida);
+}
+darBienvenida();
+
+function sumar (num1, num2)
+{
+    let resultado = num1 + num2;
+    console.log (resultado)
+}
+sumar (200, 300)
